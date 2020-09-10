039b79a03f2318f2d69cd508979c5b367b7a8950
Tushank Shingte <61382216+tushank16@users.noreply.github.com>
2020-09-10 21:56:18 +0530

    Add files via upload

---
+++ Day_3/Attributes_Assignment/Trial3.java
@@ -0,0 +1,28 @@
+package attributes_Assignment;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Trial3
+ */
+@WebServlet("/Trial3")
+public class Trial3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("book","java");
+		RequestDispatcher rd=request.getRequestDispatcher("Trial4");
+		rd.forward(request,response);
+	}
+
+}
